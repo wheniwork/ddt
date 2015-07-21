bebe21629de1428c4d72ae2477df8d82ecf1d5e6
Woody Gilk <woody.gilk@gmail.com>
2015-07-21 10:31:18 -0500

    Simplify DDT for single-host usage
    No need for postMessage or remote server cookie setting.

--- ddt.js
+++ ddt.js
@@ -4,6 +4,12 @@
  * Licensed under 3-Clause BSD.
  * Refer to the LICENCES.txt file for details.
  * For latest version, see https://github.com/deviantART/ddt
+ *
+ * Modified 2015-07-21 by When I Work, Inc:
+ *
+ * - added "days" configuration
+ * - removed image "server" cookie setting
+ * - removed postMessage sync
  */
 /* jshint eqeqeq:true, laxcomma:true, laxbreak:true */
 (function(window) {
@@ -27,14 +33,14 @@ ddt.version = 1;
 if (typeof ddt.config !== 'object') {
     ddt.config = {};
 }
-if (!ddt.config.server) {
-    ddt.config.server = '/ddt/?channels=';
-}
 if (!ddt.config.cookie) {
     ddt.config.cookie = 'ddt_watch';
 }
 if (!ddt.config.domains) {
     ddt.config.domains = [window.location.host.split('.').slice(-2).join('.')];
+}
+if (!ddt.config.days) {
+    ddt.config.days = 90;
 }
 
 // cookie helpers
@@ -50,48 +56,41 @@ util.cookie.get =  function() {
 
 // sets the cookie for all domains by loading an image from each domain
 // config: ddt.config.server, ddt.config.domains
-util.cookie.set = function() {
-    var channels = ddt.watching().join(',')
-        ,img
-        ,i;
-    for (i = 0; i < ddt.config.domains.length; i++) {
-        // UDP style, we do absolutely nothing to ensure a successful request
-        img = new Image();
-        img.src = '//' + ddt.config.domains[i] + ddt.config.server + channels;
-    }
-    util.sync(++ddt.version);
-};
-
-// deletes the cookie for the current domain
-// config: ddt.config.cookie, ddt.config.domains
-util.cookie.del = function() {
-    var expires = new Date()
+util.cookie.set = function(expires) {
+    var cookie = encodeURIComponent(ddt.config.cookie)
+        ,channels = ddt.watching.join(',')
         ,domain = window.location.host
+        ,date = new Date
         ,i;
+
+    date.setTime(date.getTime() + ((expires || ddt.config.days) * 24 * 60 * 60 * 1000));
+
     // ensure there is a matching domain to delete
     for (i = 0; i < ddt.config.domains.length; i++) {
         // typically the configured domain will be less restrictive, which is
         // why we search for the configured domain inside the current domain.
         if (domain.indexOf(ddt.config.domains[i]) !== -1) {
-            // expire 24 hours ago (in ms)
-            expires.setTime(expires.getTime() - 86400000);
-            document.cookie = i = encodeURIComponent(ddt.config.cookie) + '='
+            document.cookie = cookie + '=' + channels
                 + '; expires=' + expires.toUTCString()
                 + '; path=/'
                 + '; domain=.' + ddt.config.domains[i];
-            return util.sync(++ddt.version);
+
+            return ++ddt.version;
         }
     }
+
+    return false;
+};
+
+// deletes the cookie for the current domain
+util.cookie.del = function() {
+    return util.cookie.set(-1);
 };
 
 // get a regex that will match all domain URLs
 util.regex = function() {
     return new RegExp('^(https?:)?\\/\\/([^.]+\\.)?(' + ddt.config.domains.join('|').replace('.', '\\.') + ')\\b', 'i');
 };
-
-// sync helper, uses postMessage to sync changes to watched channels
-// starts as a noop until requirements are checked
-util.sync = function() {};
 
 // console proxy generator
 util.proxy = function(type) {
@@ -129,61 +128,6 @@ util.args = function(args) {
 util.warning = function(method, channel) {
     return console.warn('[ddt] invalid channel name', channel, 'when calling', method);
 };
-
-// attempt to enable postMessage syncing
-if (window.postMessage && typeof JSON === 'object' && typeof JSON.parse === 'function') {
-    util.sync = function(version) {
-        var  msg = JSON.stringify({ddt: true, version: version, channels: ddt.watching()})
-            ,regex_domains = util.regex()
-            ,origin = '*'
-            ,frames = document.getElementsByTagName('iframe')
-            ,frame
-            ,i;
-
-        for (i = 0; i < frames.length; i++) {
-            frame = frames[i];
-            // we only sync to frames that are part of DDT domains
-            if (frame.src && regex_domains.test(frame.src)) {
-                // and only to frames that have DDT loaded
-                if (frame.contentWindow.ddt && frame.contentWindow.ddt.version < version) {
-                    ddt.log('ddt', 'syncing channels down to', frame.src, 'v' + version);
-                    frame.contentWindow.postMessage(msg, origin);
-                }
-            }
-        }
-
-        if (IN_IFRAME && window.parent.ddt && window.parent.ddt.version < version) {
-            ddt.log('ddt', 'syncing channels up to', String(window.parent.name || window.parent.location), 'v' + version);
-            window.parent.postMessage(msg, origin);
-        }
-    };
-
-    window.addEventListener('message', function(event) {
-        var  msg = (event || {}).data
-            ,regex_domains = util.regex();
-
-        if (!msg || !event.origin || !regex_domains.test(event.origin)) {
-            return; // not a valid DDT message
-        }
-
-        try {
-            msg = JSON.parse(msg);
-        } catch (err) { /* ignore */ }
-        if (!msg || !msg.ddt || !msg.version || msg.version <= ddt.version) {
-            return; // not avalid DDT message, or already in sync
-        }
-
-        ddt.version = msg.version;
-        ddt.reset(msg.channels);
-
-        // continue the sync
-        util.sync(ddt.version);
-        ddt.log('ddt', 'updated watch list for', String(window.name || window.location), 'to v' + msg.version, ddt.watching());
-    });
-} else {
-    // no postMessage support in this browser, disable sync
-    console.warn('[ddt] postMessage and/or JSON support not available, sync disabled');
-}
 
 // create DDT within a separate closure to ensure that the watched list is
 // never directly manipulated by any utility method. if multiple DDT instances
